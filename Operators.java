39032b499bd2b82f4865c40f7e9db13edafd4813
MUGDHA CHOUDHARY <143961893+Mugdha3011@users.noreply.github.com>
2023-09-05 22:19:08 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,29 @@
+public class Operators {
+    public static void main(String args[]){
+        //performing mathematical operations 
+        int a = 23;
+        int b = 2;
+        //add
+        int sum = a + b;
+        //subtract
+        int subtract= a-b;
+        //multiply
+        int multiply = a*b;
+        //divide
+        int divide = a/b;
+        //finding remainder
+        int rem = a%b;
+        System.out.println( " sum is " + sum);
+        System.out.println("subtraction is " +  subtract);
+        System.out.println("division is " + divide);
+        System.out.println("multiplication is " + multiply);
+        System.out.println( " remainder is " + rem);
+                
+
+
+
+    
+      
+    }
+    
+}
